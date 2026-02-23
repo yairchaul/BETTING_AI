f4f6e9272c3ac129d5e490ba8d5bf4af79748ba0
yairchaul <yair.chaul@gmail.com>
2026-02-23 09:37:43 -0600

    Update telegram_bot.py

--- modules/telegram_bot.py
+++ modules/telegram_bot.py
@@ -1,11 +1,12 @@
 import requests
 from config import TELEGRAM_TOKEN, CHAT_ID
 
-def enviar_pick(msg):
+def enviar_pick:
 
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
     requests.post(url,data={
         "chat_id":939085593,
         "text":msg
+
     })
