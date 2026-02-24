203e4b030d1b412aad6da294c05deb26d3ca2174
yairchaul <yair.chaul@gmail.com>
2026-02-24 12:34:38 -0600

    Update schemas.py

--- modules/schemas.py
+++ modules/schemas.py
@@ -1,10 +1,15 @@
 from dataclasses import dataclass
 
 @dataclass
-class BetData:
-    team: str
-    odds: float
-    probability: float
+class Match:
+    local: str
+    visitante: str
+
+@dataclass
+class MatchResult:
+    partido: str
+    pick: str
+    probabilidad: int
 
 @dataclass
 class EVResult:
