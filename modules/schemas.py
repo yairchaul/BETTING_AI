e6fb63adc8666509597c3e8db9a146e9fc6b818c
yairchaul <yair.chaul@gmail.com>
2026-02-26 09:15:29 -0600

    Update schemas.py

--- modules/schemas.py
+++ modules/schemas.py
@@ -1,14 +1,5 @@
 from dataclasses import dataclass
 
-@dataclass
-class Game:
-    market: str
-    home: str
-    home_odd: str
-    draw_odd: str
-    away: str
-    away_odd: str
-
 
 @dataclass
 class Pick:
