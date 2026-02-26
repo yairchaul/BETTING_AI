5dacf9cb4dff9840e47295af48c0e53b4c313da0
yairchaul <yair.chaul@gmail.com>
2026-02-26 13:29:52 -0600

    Update google_context.py

--- modules/google_context.py
+++ modules/google_context.py
@@ -1,13 +1,7 @@
-import random
-
-
 def get_match_context(home, away):
-    """
-    Contexto externo tipo noticias / momentum.
-    """
 
     return {
-        "injuries": random.choice([0, 1]),
-        "importance": random.uniform(0.4, 1.0),
-        "public_hype": random.uniform(0.3, 1.0),
+        "injuries": False,
+        "weather": "normal",
+        "importance": 1.0
     }
