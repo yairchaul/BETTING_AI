dc1f17f1d04cb35868fab3871c748dff668ac7ba
yairchaul <yair.chaul@gmail.com>
2026-02-26 08:38:46 -0600

    Update montecarlo.py

--- modules/montecarlo.py
+++ modules/montecarlo.py
@@ -1,14 +1,19 @@
+import random
 
-import numpy as np
+def simulate_parlay(picks, simulations=500):
 
-def simular_total(media_modelo, simulaciones=10000):
-    """
-    Simulación Monte Carlo para determinar la probabilidad de 'Over'.
-    """
-    if media_modelo <= 0:
-        return 0.5
-    
-    # Genera una distribución basada en la media proyectada
-    resultados = np.random.poisson(media_modelo, simulaciones)
-    probabilidad_over = np.mean(resultados > (media_modelo - 4)) # Compara con la línea real
-    return float(probabilidad_over)
+    wins = 0
+
+    for _ in range(simulations):
+
+        success = True
+
+        for p in picks:
+            if random.random() > (p.probabilidad/100):
+                success = False
+                break
+
+        if success:
+            wins += 1
+
+    return round((wins/simulations)*100,2)
