c4c1c165af1774779f19df862d4916f5576c791a
yairchaul <yair.chaul@gmail.com>
2026-02-26 12:03:33 -0600

    Update schemas.py

--- modules/schemas.py
+++ modules/schemas.py
@@ -2,11 +2,16 @@ from dataclasses import dataclass
 from typing import List
 
 @dataclass
+class Match:
+    home: str
+    away: str
+
+@dataclass
 class PickResult:
     match: str
     selection: str
     probability: float
-    odd: float  # Nombre exacto para evitar AttributeError
+    odd: float  # <--- Corregido para evitar AttributeError
     ev: float
 
 @dataclass
