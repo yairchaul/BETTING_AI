70e3b1f973f4ca88ccb991448f1d09c0038e196f
yairchaul <yair.chaul@gmail.com>
2026-03-03 16:13:00 -0600

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,20 @@
+# modules/__init__.py
+"""
+Módulos del Analizador Profesional de Apuestas
+"""
 
+from .vision_reader import ImageParser
+from .groq_vision import GroqVisionParser
+from .universal_parser import UniversalParser
+from .smart_searcher import SmartSearcher
+from .pro_analyzer_ultimate import ProAnalyzerUltimate
+from .odds_integrator import OddsIntegrator
+from .value_detector import ValueDetector
+from .ollama_analyzer import OllamaAnalyzer
+from .parlay_optimizer import ParlayOptimizer
+from .parlay_builder import show_parlay_options
+from .betting_tracker import BettingTracker
+from .montecarlo_pro import MonteCarloPro
+from .elo_system import ELOSystem
+from .xgboost_predictor import XGBoostPredictor
+from .data_collector import DataCollector
